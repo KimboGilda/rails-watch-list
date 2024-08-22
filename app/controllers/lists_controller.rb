ec739db2d537886015a5cc891dff1baa1fec78b9
nikos bakogiannis <nbakojohn13@gmail.com>
2024-08-22 12:36:56 +0200

    add routes for model List

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,29 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    # here is a get
+    @list = List.new
+  end
+
+  def create
+    # here is a post
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list, notice: "New list successfully added!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
