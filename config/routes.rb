ac9239533e3b1fca1a5b741a381075198d6f3ab0
nikos bakogiannis <nbakojohn13@gmail.com>
2024-08-24 20:26:20 +0200

    add unnecessary feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # A user can see all the lists
   # A user can see the details of a given list and its name
   # A user can create a new list
+  resources :movies
   resources :lists do
     resources :bookmarks, only: [:new, :create, :destroy]
   end
